ba4848c1c443923e32e50444496a037ae41cf725
Oliver Abert <oliver.abert@me.com>
2016-06-09 11:28:33 +0200

    Initial Submit

---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalCamera : MonoBehaviour {
+
+	public Transform PlayerCamera;
+	public Transform PortalIn;
+	public Transform PortalOut;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 portalViewVector = PortalIn.position - PlayerCamera.position;
+		float length = portalViewVector.magnitude;
+
+		//this accounts for the rotation of the outgoing portal screen, e.g. "forward" is in direction of the portal
+		Quaternion rotationFix = PortalOut.parent.rotation;
+		portalViewVector = rotationFix * portalViewVector;
+		Vector3 newPosition = PortalOut.position + portalViewVector;
+
+		transform.position = newPosition;
+		transform.LookAt(newPosition);
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("I got started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (new Vector3 (0,1,0));
+
+	}
+}
